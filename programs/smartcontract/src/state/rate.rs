79fb4106ef31fb80cde850df38208b763a4a56a8
yebology <yobelnathaniel12@gmail.com>
2024-09-23 23:03:41 +0700

    done unit test

--- programs/smartcontract/src/state/rate.rs
+++ programs/smartcontract/src/state/rate.rs
@@ -8,11 +8,13 @@ pub struct Rate {
 }
 
 impl Rate {
+    const DISCRIMINATOR_SPACE : usize = 8;
     const USER_SPACE : usize = 32;
     const COURSE_ID_SPACE : usize = 8;
     const RATING_SPACE : usize = 8;
 
     pub const MAXIMUM_SIZE : usize = 
+    Self::DISCRIMINATOR_SPACE +
     Self::USER_SPACE + 
     Self::COURSE_ID_SPACE + 
     Self::RATING_SPACE;
--- tests/smartcontract.ts
+++ tests/smartcontract.ts
@@ -1,16 +1,93 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Smartcontract } from "../target/types/smartcontract";
+import { SystemProgram } from "@solana/web3.js";
+import { assert } from "chai";
 
 describe("smartcontract", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Smartcontract as Program<Smartcontract>;
+  const systemProgram = SystemProgram.programId;
+  const user = provider.wallet;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("can rate a course!", async () => {
+    const now = Math.floor(new Date().getTime() / 1000);
+    const id = new anchor.BN(now);
+    const rating = new anchor.BN(4);
+
+    const courseId = id.toBuffer("le", 8);
+    const [ratePda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("rate"), user.publicKey.toBuffer(), courseId],
+      program.programId
+    );
+
+    await program.methods
+      .rateCourse(id, rating)
+      .accounts({
+        rate: ratePda,
+        user: user.publicKey,
+        systemProgram: systemProgram,
+      })
+      .rpc();
+
+    const account = await program.account.rate.fetch(ratePda);
+    assert.strictEqual(account.courseId.toString(), id.toString());
+    assert.strictEqual(account.user.toString(), user.publicKey.toString());
+    assert.strictEqual(account.rating.toString(), rating.toString());
+  });
+
+  it("throw error if rating is invalid!", async () => {
+    const now = Math.floor(new Date().getTime() / 1000);
+    const id = new anchor.BN(now);
+    const rating = new anchor.BN(0);
+
+    const courseId = id.toBuffer("le", 8);
+    const [ratePda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("rate"), user.publicKey.toBuffer(), courseId],
+      program.programId
+    );
+
+    try {
+      await program.methods
+        .rateCourse(id, rating)
+        .accounts({
+          rate: ratePda,
+          user: user.publicKey,
+          systemProgram: systemProgram,
+        })
+        .rpc();
+        assert(false)
+    } catch (error) {
+      console.log(error);
+      assert(true);
+    }
+  });
+
+  it("throw error if course id is not valid!", async () => {
+    const id = new anchor.BN(0);
+    const rating = new anchor.BN(3);
+
+    const courseId = id.toBuffer("le", 8);
+    const [ratePda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("rate"), user.publicKey.toBuffer(), courseId],
+      program.programId
+    );
+
+    try {
+      await program.methods
+        .rateCourse(id, rating)
+        .accounts({
+          rate: ratePda,
+          user: user.publicKey,
+          systemProgram: systemProgram,
+        })
+        .rpc();
+        assert(false);
+    } catch (error) {
+      console.log(error);
+      assert(true);
+    }
   });
 });
